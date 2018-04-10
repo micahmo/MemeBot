79502214bbb545f42512d41a3a8dce7048cb7198
Micah Morrison <micahmorrison95@gmail.com>
2018-04-10 15:04:46 -0400

    initial commit for meme bot

--- app.py
+++ app.py
@@ -7,21 +7,12 @@ import time
 from random import randint
 import json
 
-
-# consts
-LOW_MAGIC_NUMBER = 25
-HIGH_MAGIC_NUMBER = 50
-
-UNIX_ID = -27946567
-LABMONS_ID = -4506728
-BOTTESTS_ID = -164436920
 MICAHMO_ID = 76034823
-TIM_ID = 55712750
 
 PORT = int(os.environ.get('PORT', 5000))
-TOKEN = "378332395:AAG1Brzgor5YKYAUuqtek4Tknv1xasbsJXE"
+TOKEN = "554574433:AAE6O2v3sm7yEQ9kJYW7GPp-JGnrUSyUMGM"
 SECRET = "/BOT" + TOKEN
-URL = "https://validBOT.herokuapp.com/"
+URL = "https://memebot42.herokuapp.com/"
 
 
 
@@ -41,9 +32,6 @@ def pass_update():
 
 # bot logic
 def handle(msg):
-    global num, rand_num
-    num += 1
-
     content_type, chat_type, chat_id = telepot.glance(msg)
     
     # keep track of whom the message is from
@@ -63,71 +51,11 @@ def handle(msg):
     # re-write the file
     with open('data.json', 'w') as f: # create or truncate file for writing
         json.dump(data, f)
-        
 
-    print("num " + str(num))
-    print("rand_num " + str(rand_num))
     pprint.pprint(msg)
 
-    # don't let randos use teh Bot
-    if (chat_id != UNIX_ID and
-        chat_id != LABMONS_ID and
-        chat_id != BOTTESTS_ID and
-        chat_id != TIM_ID and
-        chat_id != MICAHMO_ID): # me
-        BOT.sendMessage(chat_id, "You must receive permission from the developer, @micahmo, to use this bot.")
-        print("Sent because INVALID chat")
-        return
-
-
-    if chat_id == MICAHMO_ID or chat_id == TIM_ID:
-        if msg["text"].lower() == "help":
-            with open('data.json', 'r') as f: # file WILL exist at this point; open for reading
-                data = json.load(f)
-                chatz = ""
-                for key, value in data.items():
-                    chatz += value + "\n"
-
-                BOT.sendMessage(chat_id, "These are the chats that I know about: \n\n" + chatz + "\nSend a message to one of these chats by typing \"<ID>: <message>\"")
-        else:
-            try:
-                parts = msg["text"].split(":")
-                parts[0] = parts[0].strip()
-                parts[1] = parts[1].strip()
-                print("Sending message \"" + parts[1] + "\" to chat " + str(int(parts[0])))
-                BOT.sendMessage(int(parts[0]), parts[1])
-                BOT.sendMessage(chat_id, "Message sent!")
-            except Exception, e:
-                print("Failed to send message: " + str(e))
-        return
-    
-    if ("valid" == msg["text"].lower()):
-        BOT.sendPhoto(chat_id, open("image.jpg", "rb"))
-        print("Sent because \"valid\" was exact message of text")
-    elif (num == rand_num):
-        BOT.sendMessage(chat_id, "Valid")
-        print("Sent because we reached the random number " + str(rand_num))
-        num = 0
-        rand_num = randint(LOW_MAGIC_NUMBER, HIGH_MAGIC_NUMBER)
-    elif str(msg["from"]["id"]) == "55712750" and len(msg["text"]) > 100:
-        BOT.sendMessage(chat_id, "Valid")
-        print("Sent because long message from Tim")
-    elif len(msg["text"]) > 200:
-        BOT.sendMessage(chat_id, "Valid")
-        print("Sent because message long af")
-    elif (num % 2 == 0):
-        if ("valid" in msg["text"].lower()):
-            BOT.sendMessage(chat_id, "Valid")
-            print("Sent because \"valid\" in message text and it's an even interval")
-        elif ("nintendo" in msg["text"].lower() or "switch" in msg["text"].lower()):
-            BOT.sendMessage(chat_id, "Valid")
-            print("Sent because \"nintento\" or \"switch\" in message text and it's an even interval")
-        elif ("tfw" in msg["text"].lower()):
-            BOT.sendMessage(chat_id, "Valid")
-            print("Sent because \"tfw\" in message text and it's an even interval")
-        elif ("tim" in msg["text"].lower() and "time" not in msg["text"].lower()):
-            BOT.sendMessage(chat_id, "Valid")
-            print("Sent because \"tim\" (but not \"time\") in message text and it's an even interval")
+    if msg["text"].lower() == "help":
+        BOT.sendMessage(chat_id, "suh dude")
 
 
 # set up bot
