8fc7e2825f797a96068899fb4d125a12698d0478
Micah Morrison <micahmorrison95@gmail.com>
2018-04-12 16:34:46 -0400

    add addtl env var

--- app.py
+++ app.py
@@ -88,6 +88,9 @@ def upload_file(fileName):
     S3_BUCKET = os.environ.get('S3_BUCKET')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    
+    # set up some environment variables that boto will need
+    os.environ['S3_USE_SIGV4'] = 'True'
     os.environ['REGION_HOST'] = 'apigateway.us-east-2.amazonaws.com'
 
 
