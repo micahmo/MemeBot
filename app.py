6ebd4c68622758fd8fd01283517907fad49cc80f
Micah Morrison <micahmorrison95@gmail.com>
2018-04-12 12:52:41 -0400

    add region host to environment variables' git push herok

--- app.py
+++ app.py
@@ -88,9 +88,11 @@ def upload_file(fileName):
     S3_BUCKET = os.environ.get('S3_BUCKET')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    os.environ['REGION_HOST'] = 'apigateway.us-east-2.amazonaws.com'
+
 
     # get the connection
-    conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, host='apigateway.us-east-2.amazonaws.com')
+    conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, host=os.environ.['REGION_HOST'])
     
     # get the bucket
     bucket = conn.get_bucket(S3_BUCKET)
