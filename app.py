cee2d266b1aac5f7b793147becef671557b145ca
Micah Morrison <micahmorrison95@gmail.com>
2018-04-28 16:53:08 -0400

    give document title

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ def handleInline(msg):
             photos.append(InlineQueryResultCachedPhoto(id=fileId, photo_file_id=fileId))
         elif ("animation" in file.get("file_path")):
             # it's a gif
-            photos.append(InlineQueryResultCachedDocument(id=fileId, document_file_id=fileId))
+            photos.append(InlineQueryResultCachedDocument(id=fileId, document_file_id=fileId, title=meme_data[fileId].name))
 
         #print("file with id {} is ".format(fileId))
         #pprint.pprint(file)
