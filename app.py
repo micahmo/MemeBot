03226f2e988c5762f5f1443411d819c84d03f6f0
Micah Morrison <micahmorrison95@gmail.com>
2018-04-29 09:47:34 -0400

    handle case where we get gif but are waiting for name

--- app.py
+++ app.py
@@ -221,6 +221,9 @@ def handleChat(msg):
                 else:
                     BOT.sendMessage(chat_id, "Hmm, I'm not sure what you want. :( Feel free to send me a new meme with /addmeme!")
             
+            elif message_status.get(chat_id) == MessageStatus.WaitingForMemeName: # we're waiting for a meme name, but they didn't sent a picture...
+                BOT.sendMessage(chat_id, "Hmm, I'm still waiting for you to send me a name for the meme...")
+            
             else:
                 BOT.sendMessage(chat_id, "At this time, the file format that you sent ({}) is not supported. :( Feel free to contact the developer @micahmo to add support for this format.".format(msg.get("document").get("mime_type")))
 
